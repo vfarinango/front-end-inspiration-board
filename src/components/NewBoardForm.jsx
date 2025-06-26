fdd0171e9ab6724a1423d4fad2f9f20b09651146
Shirley425 <tanqiaoqiao425@gmail.com>
2025-06-25 19:40:55 -0700

    finished board and card, still need to work on app

---
+++ src/components/NewBoardForm.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+import "./NewBoardForm.css";
+
+const NEW_BOARD = {
+  board_id: "",
+  title: "",
+  owner: "",
+};
+
+const NewBoardForm = ({ addBoardCallback }) => {
+  const [boardData, setBoardData] = useState(NEW_BOARD);
+
+  const submitBoardData = (e) => {
+    e.preventDefault();
+
+    addBoardCallback(boardData);
+    setBoardData(NEW_BOARD);
+  };
+
+  const handleChange = (e) => {
+    setBoardData({ ...boardData, [e.target.name]: e.target.value });
+  };
+
+    return (
+    <form onSubmit={submitBoardData} className='newBoardForm'>
+      <section>
+        <h2>CREATE A NEW Board</h2>
+        <div className='newBoardFields'>
+          <label htmlFor='Title'>Title</label>
+          <input 
+          id='title'
+          name='title'
+          value={boardData.title}
+          onChange={handleChange}
+          />
+
+          <label htmlFor='OwnerName'>Owner's Name</label>
+          <input 
+          id='owner'
+          name='owner'
+          value={boardData.owner}
+          onChange={handleChange}
+          />
+
+          <p>Preview: {boardData.title} - {boardData.owner}</p>
+          
+          <button className='button newBoardSubmit' type='submit'>
+            Submit
+          </button>
+        </div>
+      </section>
+    </form>
+    );
+};
+
+NewBoardForm.propTypes = {
+  addBoardCallback: PropTypes.func.isRequired,
+};
+
+export default NewBoardForm;
---
+++ src/components/NewCardForm.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+import "./NewCardForm.css";
+
+const NEW_CARD = {
+  message: "",
+};
+
+const NewCardForm = ({ addCardCallback }) => {
+  const [cardData, setCardData] = useState(NEW_CARD);
+
+  const submitCardData = (e) => {
+    e.preventDefault();
+
+    addCardCallback(CardData);
+    setCardData(NEW_CARD);
+  };
+
+  const handleChange = (e) => {
+    setCardData({ ...CardData, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <form onSubmit={submitCardData} className='newCardForm'>
+      <section>
+        <h2>CREATE A NEW CARD</h2>
+        <div className='newCardFields'>
+          <label htmlFor='Message'>Message</label>
+          <input
+          id='message'
+          name='message'
+          value={cardData.message}
+          onChange={handleChange}
+          />
+
+          <p>Preview: {cardData.message}</p>
+
+          <button className='button newCardSubmit' type='submit'>
+            Submit
+          </button>
+        </div>
+      </section>
+    </form>
+  );
+};
+
+NewCardForm.propTypes = {
+  addCardCallback: PropTypes.func.isRequired,
+};
+
+export default NewCardForm;
