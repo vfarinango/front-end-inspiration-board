a16ab6a8bd3f2bbfe03e808f349fd5435ee9e9a8
Shirley425 <tanqiaoqiao425@gmail.com>
2025-06-25 21:34:40 -0700

    updated App

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,98 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
+import NewBoardForm from './components/NewBoardForm'
+import NewCardForm from './components/NewCardForm'
+
+const URL = 'TBD';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [boards, setBoards] = useState([]);
+  const [cards, setCards] = useState([]);
+  const [status, setStatus] = useState('Loading...');
+
+
+  const addBoard = (boardData) => {
+    axios
+      .post(`${URL}/boards`, boardData)
+      .then((response) => {
+        const newBoard = response.data;
+        setBoards([...boards, newBoard]);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const addCard = (cardData) => {
+    axios
+      .post(`${URL}/cards`, cardData)
+      .then((response) => {
+        const newCard = response.data;
+        setCards([...cards, newCard]);
+      })
+      .catch((error) => console.log(error));
+  };
+
+
+  useEffect(() => {
+    axios
+      .get(URL)
+      .then((response) => {
+        const newBoards = response.data.map((board) => {
+          return {
+            board_id: board.id,
+            title: board.title,
+            owner: board.owner
+          };
+        });
+        setStatus('Loaded');
+        setBoards(newBoards);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(URL)
+      .then((response) => {
+        const newCards = response.data.map((card) => {
+          return {
+            card_id: card.id,
+            message: card.title,
+            likes_count: card.likes_count,
+            // board_id: ?
+          };
+        });
+        setStatus('Loaded');
+        setCards(newCards);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className='App'>
+      <header className='App-header'>
+        <h1>INSPIRATION BOARD</h1>
+      </header>
+      <main>
+        <div>
+          <NewBoardForm>
+            <NewBoardForm addBoardCallback={addBoard} />
+          </NewBoardForm>
+        </div>
+        <div>
+          <NewCardForm>
+            <NewCardForm addCardCallback={addCard} />
+          </NewCardForm>
+        </div>
+      </main>
+    </div>
+  );
+};
 
 export default App
