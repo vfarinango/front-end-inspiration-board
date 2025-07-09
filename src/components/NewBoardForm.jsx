d4061cfaa9de103c848944100517224a91413ce1
Shirley425 <tanqiaoqiao425@gmail.com>
2025-07-09 15:15:11 -0700

    fixed NewBoardForm

--- src/components/NewBoardForm.jsx
+++ src/components/NewBoardForm.jsx
@@ -9,10 +9,10 @@ const NEW_BOARD = {
 
 const NewBoardForm = ({ addBoardCallback }) => {
   const [boardData, setBoardData] = useState(NEW_BOARD);
+  const [showForm, setShowForm] = useState(true);
 
   const submitBoardData = (e) => {
     e.preventDefault();
-
     addBoardCallback(boardData);
     setBoardData(NEW_BOARD);
   };
@@ -21,40 +21,52 @@ const NewBoardForm = ({ addBoardCallback }) => {
     setBoardData({ ...boardData, [e.target.name]: e.target.value });
   };
 
-    return (
+  const toggleForm = () => {
+    setShowForm(!showForm);
+  };
+
+  return (
     <form onSubmit={submitBoardData} className='newBoardForm'>
       <section>
         <h2>CREATE A NEW Board</h2>
-        <div className='newBoardFields'>
-          <label htmlFor='Title'>Title</label>
-          <input 
-          id='title'
-          name='title'
-          value={boardData.title}
-          onChange={handleChange}
-          />
 
-          <label htmlFor='OwnerName'>Owner's Name</label>
-          <input 
-          id='owner'
-          name='owner'
-          value={boardData.owner}
-          onChange={handleChange}
-          />
+        {showForm && (
+          <div className='newBoardFields'>
+            <label htmlFor='Title'>Title</label>
+            <input
+              id='title'
+              name='title'
+              value={boardData.title}
+              onChange={handleChange}
+            />
 
-          <p>Preview: {boardData.title} - {boardData.owner}</p>
-          
-          <button className='button newBoardSubmit' type='submit'>
-            Submit
-          </button>
-          {/* Need to add toggle function */}
-          <button className='toggleHideForm'>  
-            Hide New Board Form
-          </button>
-        </div>
+            <label htmlFor='OwnerName'>Owner's Name</label>
+            <input
+              id='owner'
+              name='owner'
+              value={boardData.owner}
+              onChange={handleChange}
+            />
+
+            <p>
+              Preview: {boardData.title} - {boardData.owner}
+            </p>
+            <button 
+              className='button newBoardSubmit' 
+              type='submit'>
+              Submit
+            </button>
+          </div>
+        )}
+        <button 
+          type='button' 
+          className='toggleHideForm' 
+          onClick={toggleForm}>
+          {showForm ? "Hide New Board Form" : "Show New Board Form"}
+        </button>
       </section>
     </form>
-    );
+  );
 };
 
 export default NewBoardForm;
